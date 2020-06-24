51aade598fdc7f299a5f98c1eb9d38df2cceb8ed
mcirstea92 <mariuscirstea92@gmail.com>
2020-06-24 12:32:33 +0300

    Add sum function for query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,6 +6,13 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
+
+        if (query.contains("what is") && query.contains("plus")) {
+            int first = Integer.parseInt(query.substring(8, query.indexOf("plus") - 1));
+            int second = Integer.parseInt(query.substring(query.indexOf("plus") + 5));
+            return "" + (first + second);
+        }
+
         return "";
     }
 }

6341a3f6c64f080d4baec8760534db2e8ff7e457
mcirstea92 <mariuscirstea92@gmail.com>
2020-06-24 12:47:05 +0300

    Add largest number query validation

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,6 +1,13 @@
 package com.develogical;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class QueryProcessor {
+
+    private static final String LARGEST = "which of the following numbers is the largest";
 
     public String process(String query) {
         if (query.toLowerCase().contains("romeo and juliet")) {
@@ -11,6 +18,18 @@ public class QueryProcessor {
             int first = Integer.parseInt(query.substring(8, query.indexOf("plus") - 1));
             int second = Integer.parseInt(query.substring(query.indexOf("plus") + 5));
             return "" + (first + second);
+        }
+
+        if (query.contains(LARGEST)) {
+            String numbers = query.substring(LARGEST.length() + 1);
+            System.out.println("Numbers are " + numbers);
+            String[] parsedStringNumbers = numbers.split(",");
+            List<Integer> ints = new ArrayList<>();
+            for (String parsedStringNumber : parsedStringNumbers) {
+                ints.add(Integer.parseInt(parsedStringNumber));
+            }
+            System.out.println("Largest number is " + Collections.max(ints));
+            return "" + Collections.max(ints);
         }
 
         return "";
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -8,20 +8,30 @@ import static org.junit.Assert.assertThat;
 
 public class QueryProcessorTest {
 
-  QueryProcessor queryProcessor = new QueryProcessor();
+    QueryProcessor queryProcessor = new QueryProcessor();
 
-  @Test
-  public void knowsWhoWroteRomeoAndJuliet() throws Exception {
-    assertThat(queryProcessor.process("Who wrote Romeo And Juliet"), containsString("Shakespeare"));
-  }
+    @Test
+    public void knowsWhoWroteRomeoAndJuliet() throws Exception {
+        assertThat(queryProcessor.process("Who wrote Romeo And Juliet"), containsString("Shakespeare"));
+    }
 
-  @Test
-  public void isNotCaseSensitive() throws Exception {
-    assertThat(queryProcessor.process("who wrote romeo and juliet"), containsString("Shakespeare"));
-  }
+    @Test
+    public void isNotCaseSensitive() throws Exception {
+        assertThat(queryProcessor.process("who wrote romeo and juliet"), containsString("Shakespeare"));
+    }
 
-  @Test
-  public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-    assertThat(queryProcessor.process("test"), is(""));
-  }
+    @Test
+    public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
+        assertThat(queryProcessor.process("test"), is(""));
+    }
+
+    @Test
+    public void returns2040when2020plus20() throws Exception {
+        assertThat(queryProcessor.process("what is 2020 plus 20"), is("2040"));
+    }
+
+    @Test
+    public void returns2040whenlargest2020_2040() throws Exception {
+        assertThat(queryProcessor.process("which of the following numbers is the largest:2020,2040"), is("2040"));
+    }
 }
